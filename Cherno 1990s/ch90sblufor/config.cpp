004b667dd1a35d710b2540bfe6a7626f12d31ae0
Carl Mylo <carl@carlmylo.com>
2024-04-24 12:21:19 -0500

    Cherno 90s Faction

---
+++ Cherno 1990s/ch90sblufor/config.cpp
@@ -0,0 +1,5 @@
+#include "BIS_AddonInfo.hpp"
+#include "CfgPatches.hpp"
+#include "autogen.hpp"
+#define mag_xx(a,b) class _xx_##a {magazine = ##a; count = b;}
+#define weap_xx(a,b) class _xx_##a {weapon = ##a; count = b;}
---
+++ Cherno 1990s/ch90sopfor/BIS_AddonInfo.hpp
@@ -0,0 +1,5 @@
+class BIS_AddonInfo
+{
+	author="Carl Mylo";
+	timepacked="1711307297";
+};
